5cc335e371995f9220b68b91061042c877233d3b
Lokhandegaurav185 <gdlokhande2001@gmail.com>
2026-07-20 17:21:35 +0530

    Create generic response successfully

--- src/main/java/com/code/exception/GlobalExceptionHandler.java
+++ src/main/java/com/code/exception/GlobalExceptionHandler.java
@@ -6,31 +6,38 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.code.validation.CommonGenericResponseUtil;
+
 @ControllerAdvice
 public class GlobalExceptionHandler{
 	
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<?> handleNullPointException(Exception e){
-		return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+		return CommonGenericResponseUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+//		return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<?> handleResourceNotException(Exception e){
-		return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
+		return CommonGenericResponseUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.NOT_FOUND);
+//		return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
 	}
 	
 	@ExceptionHandler(DtoValidationException.class)
 	public ResponseEntity<?> handleDtoValidationException(DtoValidationException e){
-		return new ResponseEntity<>(e.getErrors(),HttpStatus.BAD_REQUEST);
+		return CommonGenericResponseUtil.createErrorResponse(e.getErrors(), HttpStatus.BAD_REQUEST);
+//		return new ResponseEntity<>(e.getErrors(),HttpStatus.BAD_REQUEST);
 	}
 	
 	@ExceptionHandler(ExistDataException.class)
 	public ResponseEntity<?> handleExistDataException(ExistDataException e){
-		return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+		return CommonGenericResponseUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.CONFLICT);
+//		return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
 	}
 	
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	public ResponseEntity<?> handleMessageNotReadableException(HttpMessageNotReadableException e){
-		return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+		return CommonGenericResponseUtil.createErrorResponseMessage(e.getMessage(), HttpStatus.BAD_REQUEST);
+//		return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
 	}
 }
---
+++ src/main/java/com/code/validation/CommonGenericResponseUtil.java
@@ -0,0 +1,40 @@
+package com.code.validation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.code.handler.GenericResponse;
+
+public class CommonGenericResponseUtil {
+	public static ResponseEntity<?> createBuildResponse(Object data, HttpStatus status){
+		GenericResponse response = GenericResponse.builder()
+				.responseStatus(status).status("success").message("success").data(data)
+				.build();
+		
+		return response.create();
+	}
+	
+	public static ResponseEntity<?> createBuildResponseMessage(String message, HttpStatus status){
+		GenericResponse response = GenericResponse.builder()
+				.responseStatus(status).status("success").message(message)
+				.build();
+		
+		return response.create();
+	}
+	
+	public static ResponseEntity<?> createErrorResponse(Object data, HttpStatus status){
+		GenericResponse response = GenericResponse.builder()
+				.responseStatus(status).status("failed").message("failed").data(data)
+				.build();
+		
+		return response.create();
+	}
+	
+	public static ResponseEntity<?> createErrorResponseMessage(String message, HttpStatus status){
+		GenericResponse response = GenericResponse.builder()
+				.responseStatus(status).status("failed").message(message)
+				.build();
+		
+		return response.create();
+	}
+}
